8ec5af692c7e6c25acb7e169b438679b37c0252e
Maripia Kuti <pionste@gmail.com>
2020-01-25 10:09:04 +0000

    fixed edit_record function

--- mongo_project.py
+++ mongo_project.py
@@ -86,7 +86,7 @@ def edit_record():
                 update_doc[k] = input(k.capitalize() + "[" + v + "] > ")  # show current value of k
 
                 if update_doc[k] == "":
-                    update_doc[k] == v
+                    update_doc[k] = v
 
         try:
             coll.update_one(doc, {'$set': update_doc})
